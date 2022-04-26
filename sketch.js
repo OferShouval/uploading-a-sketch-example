293dd7879e1862ca611a78ce81d57603029d810e
OferShouval <45494179+OferShouval@users.noreply.github.com>
2022-04-26 17:51:14 -0400

    made some changes

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ let prevY = 0;
 let totalRotation = 0;
 
 function setup() {
- 
+
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(220,30);
   //  fill(0,0)
   // stroke(255);
   currentX = lerp(currentX, targetX, 0.1);
@@ -34,16 +34,16 @@ function wheel(x, y, diameter) {
   //problems to solve:
 
   //not calculating the correct distance, we need distance travelled     over a single frame.
-  
+
   //Solution - create new variables that store the previous values of x and y.
 
   //wheel not rotating correctly, because we aren't giving passing the right parameters into calcAngle
-  
+
   // Solutions - divide (distanceTravelled/radius) to get rotations in radians.create a global variable totalRotations to accumulate rotations
 
   //wheel always spinning in one direction.
   //solution - if statement that checks which direction it is moving along the X axis. Can this be improved?
-  
+
 
   var dist = round(calcDistance(prevX, prevY, x, y), 2);
 
@@ -53,6 +53,7 @@ function wheel(x, y, diameter) {
 
   translate(x, y);// move the wheel towards the target. we get x,y from the lerp function
   rotate(totalRotation); //rotating the wheel by applying the cumulative total rotations to a rotate transformation
+  fill(255,0,30);
   ellipse(0, 0, diameter, diameter);// try changing your wheel design and adjusting you're background/fill opacity. See what kinds of patterns you can form.
   line(0, 0, 0, diameter / 2);
 }
@@ -74,7 +75,7 @@ function calcAngle(distance, radius) {
 }
 
 function mouseDragged() {
-  
+
   //updating targets on mouseDragged
   targetX = mouseX;
   targetY = mouseY;
